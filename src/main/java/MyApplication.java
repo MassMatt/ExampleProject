02ccafdee17eb6eadf5bc356bc399abb3fd96193
mweller <Mweller@mass.co.uk>
2018-07-13 13:45:52 +0100

    Update with rdn generator

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -1,42 +1,50 @@
+import java.util.Random;
 
 public class MyApplication {
 
-    public MyApplication(){
+    public MyApplication() {
         printHelloWorld();
-        doMath(4,3);
+        doMath(4, 3);
+
+        int numberOfWeeks = 101;
+        int tracker = 0;
+
+
+        while (numberOfWeeks > 80) {
+
+            int count = 0;
+            while (rdn(14000000) != 1) {
+                count++;
+            }
+
+
+            count = count / 2;
+
+            numberOfWeeks = count / 52;
+
+            tracker++;
+            System.out.println(tracker + " : " + numberOfWeeks);
+        }
+
+
+
     }
 
-    private void printHelloWorld () {
+    private void printHelloWorld() {
         System.out.println("Hello World");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-
     }
 
-    private void doMath (int number1, int number2) {
+    private void doMath(int number1, int number2) {
         System.out.println(number1 + number2);
     }
 
+    private int rdn(int max) {
+
+        Random rand = new Random();
+
+        int n = rand.nextInt(max) + 1;
+
+        return n;
+    }
 
 }
